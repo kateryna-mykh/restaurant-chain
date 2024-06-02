a873c35db1867ab14cb09b23df257ca843410bf6
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-06-02 14:05:21 +0300

    initial commit

---
+++ message-dispatcher/src/test/java/com/katerynamykh/task/dispatcher/MessageDispatcherApplicationTests.java
@@ -0,0 +1,11 @@
+package com.katerynamykh.task.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageDispatcherApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
