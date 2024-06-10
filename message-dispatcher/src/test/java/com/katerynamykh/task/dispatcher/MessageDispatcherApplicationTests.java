ef4d663df84842bb4f25fe5702590c260739e829
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-06-10 19:13:29 +0300

    add tests

--- message-dispatcher/src/test/java/com/katerynamykh/task/dispatcher/MessageDispatcherApplicationTests.java
+++ message-dispatcher/src/test/java/com/katerynamykh/task/dispatcher/MessageDispatcherApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MessageDispatcherApplicationTests {
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
